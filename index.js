281945a9782e631b2ce047af7289efd594dd31b3
Fatuma Dahir <faridhfatush2024@gmail.com>
2025-04-14 11:26:58 +0300

    Lab complete

--- index.js
+++ index.js
@@ -1,2 +1,24 @@
 
 // Write your code here
+// Create a variable called multiply set to an equation that will multiply the variables num1 and num2; the result of the multiplication should be 62.
+let num1 = 2;
+let num2 = 31;
+const multiply = num1 * num2;
+
+console.log(multiply); 
+
+//Create a variable called random that will generate a random integer greater than 0.
+const random = Math.floor(Math.random() * 100) + 1;
+console.log(random);
+
+// Create a variable called mod set to an equation that will calculate the remainder of dividing variable num3 by num4; the remainder should be 4.
+
+let num3 = 28
+let num4 = 6
+const mod = num3 % num4
+console.log(mod)
+
+// Create a variable called max that finds the highest number in a set; the value returned should be 20
+const max = Math.max(8,12,16,20)
+console.log(max)
+
